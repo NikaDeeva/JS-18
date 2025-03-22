036739b9bfe55e4d52346e6525ad9a9c3b5bfbb0
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2025-03-22 09:51:42 +0100

    ok

--- src/index.js
+++ src/index.js
@@ -1,55 +1,42 @@
-"use strict"
-const BASE_URL = 'http://localhost:3000/students';
-const getBtn = document.querySelector('#get-students-btn');
-const postNew = document.querySelector('#add-student-form');
-const studentsTable = document.querySelector('#students-table');
+"use strict";
+const BASE_URL = "http://localhost:3000/students";
+const getBtn = document.querySelector("#get-students-btn");
+const postNew = document.querySelector("#add-student-form");
+const studentsTable = document.querySelector("#students-table");
 
-// Отримання студентів
-function getStudents() {
-    return fetch(BASE_URL)
-        .then(response => {
-            if (!response.ok) throw new Error('Помилка завантаження студентів');
-            return response.json();
-        })
-        .catch(error => console.error(error));
+async function getStudents() {
+    try {
+        const r = await fetch(BASE_URL);
+        const data = await r.json();
+        console.log(data);
+        renderStudents(data);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-// const fetchStudents = async () => {
-//     const response = await fetch('http://localhost:3000/students');
-//           const students = await response.json();
-//           return students;
-// }
-// const getStudents = async () => {
-//     try {
-//         const students = await fetchStudents();
-//         console.log(students);
-//     }
-//     catch (error) {
-// console.log(error);
-//     }
-// };
-// getStudents();
-// Функція рендерингу студентів
 function renderStudents(students) {
     if (!Array.isArray(students)) return console.error(students);
 
-    studentsTable.innerHTML = students.map(student => `
+    studentsTable.innerHTML = students
+        .map(
+            (student) => `
         <tr data-id="${student.id}">
           <td>${student.id}</td>
           <td>
-           <p class="currentName"> ${student.name} </p>
+           <p class="currentName">${student.name}</p>
             <input type="text" class="newName" data-id="${student.id}" placeholder="Нове ім'я">
           </td>
           <td>
-           <p class="currentCourse"> ${student.course}</p>
+           <p class="currentCourse">${student.course}</p>
             <input type="text" class="newCourse" data-id="${student.id}" placeholder="Новий курс">
           </td>
           <td>
-           <p class="currentSkills"> ${student.skills}</p>
+           <p class="currentSkills">${student.skills}</p>
             <input type="text" class="newSkills" data-id="${student.id}" placeholder="Нові навички">
           </td>
           <td>
-           <p class="currentEmail"> ${student.email}</p>
+           <p class="currentEmail">${student.email}</p>
             <input type="email" class="newEmail" data-id="${student.id}" placeholder="Новий email">
           </td>
           <td>
@@ -61,68 +48,83 @@ function renderStudents(students) {
             <button class="deleteBtn" data-id="${student.id}">Видалити</button>
           </td>
         </tr>
-      `).join('');
+      `
+        )
+        .join("");
 
-    document.querySelectorAll('.updateBtn').forEach(button => {
-        button.addEventListener('click', handleUpdateStudent);
+    document.querySelectorAll(".updateBtn").forEach((button) => {
+        button.addEventListener("click", (e) => {
+            const studentId = e.target.dataset.id;
+            updateStudent(studentId, {
+                name: document.querySelector(`.newName[data-id='${studentId}']`).value || document.querySelector(`tr[data-id='${studentId}'] .currentName`).textContent,
+                course: document.querySelector(`.newCourse[data-id='${studentId}']`).value || document.querySelector(`tr[data-id='${studentId}'] .currentCourse`).textContent,
+                skills: document.querySelector(`.newSkills[data-id='${studentId}']`).value || document.querySelector(`tr[data-id='${studentId}'] .currentSkills`).textContent,
+                email: document.querySelector(`.newEmail[data-id='${studentId}']`).value || document.querySelector(`tr[data-id='${studentId}'] .currentEmail`).textContent,
+                isEnrolled: document.querySelector(`.newIsEnrolled[data-id='${studentId}']`).checked,
+            });
+        });
     });
 
-    document.querySelectorAll('.deleteBtn').forEach(button => {
-        button.addEventListener('click', handleDeleteStudent);
+    document.querySelectorAll(".deleteBtn").forEach((button) => {
+        button.addEventListener("click", (e) => deleteStudent(e.target.dataset.id));
     });
 }
 
-getBtn.addEventListener('click', 
-    getStudents()
-);
+getBtn.addEventListener("click", () => getStudents());
 
-postNew.addEventListener('submit', (e) => {
+async function addStudent(student) {
+    try {
+        const r = await fetch(`${BASE_URL}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(student),
+        });
+        const data = await r.json();
+        console.log(data);
+        getStudents(); 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+postNew.addEventListener("submit", (e) => {
     e.preventDefault();
-    const newStudent = {
-        name: document.querySelector('#name').value,
-        course: document.querySelector('#course').value,
-        skills: document.querySelector('#skills').value,
-        email: document.querySelector('#email').value,
-        isEnrolled: document.querySelector('#isEnrolled').checked,
-    };
-    fetch(BASE_URL, {
-        method: 'POST',
-        body: JSON.stringify(newStudent),
-        headers: { "Content-Type": "application/json" }
-    })
-    .then(response => response.json())
-    .then(() => getStudents().then(renderStudents))
-    .catch(error => console.error(error));
+    addStudent({
+        name: document.querySelector("#name").value,
+        course: document.querySelector("#course").value,
+        skills: document.querySelector("#skills").value,
+        email: document.querySelector("#email").value,
+        isEnrolled: document.querySelector("#isEnrolled").checked,
+    });
 });
 
-function handleUpdateStudent(e) {
-    e.preventDefault();
-    const studentId = e.target.dataset.id;
-    const updatedSt = {
-        name: document.querySelector(`.newName[data-id='${studentId}']`).value || document.querySelector('.currentName').textContent,
-        course: document.querySelector(`.newCourse[data-id='${studentId}']`).value || document.querySelector('.currentCourse').textContent,
-        skills: document.querySelector(`.newSkills[data-id='${studentId}']`).value || document.querySelector('.currentSkills').textContent,
-        email: document.querySelector(`.newEmail[data-id='${studentId}']`).value || document.querySelector('.currentEmail').textContent,
-        isEnrolled: document.querySelector(`.newIsEnrolled[data-id='${studentId}']`).checked,
-    };
-    
-    fetch(`${BASE_URL}/${studentId}`, {
-        method: 'PUT',
-        body: JSON.stringify(updatedSt),
-        headers: { "Content-Type": "application/json" }
-    })
-    .then(response => {
-        if (!response.ok) throw new Error('Помилка оновлення студента');
-        return response.json();
-    })
-    .then(() => getStudents().then(renderStudents))
-    .catch(error => console.error(error));
-}
+async function updateStudent(id, patched) {
+    try {
+        const r = await fetch(`${BASE_URL}/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(patched),
+        });
+        const data = await r.json();
+        console.log(data);
+        getStudents(); 
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-function handleDeleteStudent(e) {
-    e.preventDefault();
-    const studentId = e.target.dataset.id;
-    fetch(`${BASE_URL}/${studentId}`, { method: 'DELETE'})
-    .then(() => getStudents().then(renderStudents))
-    .catch(error => console.error(error));
-}
+async function deleteStudent(id) {
+    try {
+        await fetch(`${BASE_URL}/${id}`, {
+            method: "DELETE",
+        });
+        console.log(id);
+        getStudents();
+    } catch (error) {
+        console.log(error);
+    }
+};
